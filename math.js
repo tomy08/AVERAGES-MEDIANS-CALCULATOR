a95422c299f2d88ed850b32b11a511362af5e0c3
Tomas <tomasesantacruz@gmail.com>
2022-09-13 21:00:10 -0300

    Cambie objeto MathFunction por clase y solucion de problemas del agregado de inputs

--- math.js
+++ math.js
@@ -1,37 +1,37 @@
-const MathFunction = {};
+class MathFunction {
+  static isPair = (lista) => {
+    return !(lista.length % 2);
+  };
 
-MathFunction.isPair = (lista) => {
-  return !(lista.length % 2);
-};
+  static calculatorMedian = (listaDesordenada) => {
+    const lista = MathFunction.tidyList(listaDesordenada);
+    const listaisPair = MathFunction.isPair(lista);
 
-MathFunction.calculatorMedian = (listaDesordenada) => {
-  const lista = MathFunction.tidyList(listaDesordenada);
-  const listaisPair = MathFunction.isPair(lista);
+    if (listaisPair) {
+      const indexMedium1 = lista.length / 2 - 1;
+      const indexMedium2 = lista.length / 2;
+      const mediumlists = [];
+      mediumlists.push(lista[indexMedium1]);
+      mediumlists.push(lista[indexMedium2]);
 
-  if (listaisPair) {
-    const indexMedium1 = lista.length / 2 - 1;
-    const indexMedium2 = lista.length / 2;
-    const mediumlists = [];
-    mediumlists.push(lista[indexMedium1]);
-    mediumlists.push(lista[indexMedium2]);
+      const medianPair = MathFunction.calculatorAverage(mediumlists);
+      return medianPair;
+    } else {
+      const indexMedium = Math.floor(lista.length / 2);
+      const medianImpar = lista[indexMedium];
+      return medianImpar;
+    }
+  };
 
-    const medianPair = MathFunction.calculatorAverage(mediumlists);
-    return medianPair;
-  } else {
-    const indexMedium = Math.floor(lista.length / 2);
-    const medianImpar = lista[indexMedium];
-    return medianImpar;
-  }
-};
+  static calculatorAverage = (lista) => {
+    const plusList = lista.reduce((a, b) => a + b);
 
-MathFunction.calculatorAverage = (lista) => {
-  const plusList = lista.reduce((a, b) => a + b);
+    const average = plusList / lista.length;
+    return average;
+  };
 
-  const average = plusList / lista.length;
-  return average;
-};
-
-MathFunction.tidyList = (listaDisordenada) => {
-  const list = listaDisordenada.sort((a, b) => a - b);
-  return list;
-};
+  static tidyList = (listaDisordenada) => {
+    const list = listaDisordenada.sort((a, b) => a - b);
+    return list;
+  };
+}
